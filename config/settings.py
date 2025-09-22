acf7bf5078a801e3a66b6d2a0a7b0827ecca0acb
Anita <anitatam2001@gmail.com>
2025-09-22 14:49:44 +0800

    Step 2 : save key into .env

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.getenv('SITE_SECRET_KEY')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n@#9y37sp@=dq#h98-7e&c2*p8gidfj4$1=$9(f9$u5)!pz+of'
+SITE_SECRET_KEY = 'django-insecure-n@#9y37sp@=dq#h98-7e&c2*p8gidfj4$1=$9(f9$u5)!pz+of'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
