4a7f447c590f268f494336d039f1661478855690
Anita <anitatam2001@gmail.com>
2025-09-26 14:25:02 +0800

    step 6 : navbar links update

--- pages/views.py
+++ pages/views.py
@@ -6,7 +6,18 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'pages/index.html')
 
-
 def about(request):
     return render(request, 'pages/about.html')
+
+def listings(request):
+    return render(request, 'pages/listings.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+
+
 
