74023d03bdfde1a1b8c17558b25f095ca568665f
Anita <anitatam2001@gmail.com>
2025-10-06 12:59:06 +0800

    Step 13 : listing page.

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-# Create your views here.
 def listings(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True) 
     #listings = Listing.objects.all()
@@ -10,9 +9,10 @@ def listings(request):
     paged_listings = paginator.get_page(page) 
     context = {'listings': paged_listings} 
     return render(request, 'listings/listings.html', context)
-
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request,listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    context = {'listing':listing}
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
