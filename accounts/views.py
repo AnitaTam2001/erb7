68a7256f3d8eba190d4e3d7426c19784c97dadf5
Anita <anitatam2001@gmail.com>
2025-10-13 17:54:14 +0800

    Step 17: register.logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 # Create your views here.
 def register(request):
@@ -30,9 +30,23 @@ def register(request):
         return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request, "You are now logged in")
+            return redirect('accounts:dashboard')
+        else:
+            messages.error(request,"Invalid credentials")
+            return redirect('accounts:login')
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
+    if request.method == "POST":
+        auth.logout(request)
     return redirect('pages:index')
 
 def dashboard(request):
