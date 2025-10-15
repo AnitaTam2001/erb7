a6820059289ec59b24f25d2fe2a33ae7f5a2a7fe
Anita <anitatam2001@gmail.com>
2025-10-15 15:34:54 +0800

    Step 19: final fixed up of listing. html

--- contacts/urls.py
+++ contacts/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('contact/delete/<int:contact_id>', views.delete_contact, name='delete_contact'),
     path('contact/edit/<int:contact_id>', views.edit_contact, name='edit_contact'),
-
-
 ]
---
+++ templates/readme.md
@@ -0,0 +1,14 @@
+# Django Clinic Project
+## Steps to build the project
+### 1. Create virtual environment
+```bash
+mkvirtualenv erb7
+```
+
+### 2. create project folder
+
+```hash
+django-admin startproject erb7 .
+```
+
+
